d50b82d5a79cb1029426976c6db03f3ca1099c5f
apiga <ambroise.piganeau@gmail.com>
2015-11-05 17:28:35 -0800

    modified db url

--- appMongo/config.js
+++ appMongo/config.js
@@ -1,7 +1,7 @@
 // var MongoClient = require('mongodb').MongoClient;
 var mongoose = require('mongoose');
 
-var url = 'mongodb://localhost:27017/shortly';
+var url = process.env.production ? 'mongodb://MongoLab-9:1lDpy0un8RzgnpxNMzif371nh_lkaQblAmF.D1jiwLM-@ds048368.mongolab.com:48368/MongoLab-9' : 'mongodb://localhost:27017/shortly';
 mongoose.connect(url);
 
 var db = mongoose.connection;
